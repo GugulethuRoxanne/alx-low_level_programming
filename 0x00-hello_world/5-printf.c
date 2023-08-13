303b06f0b0969dc5ee7a04d6c9f6ab84a61ba53d
GugulethuRoxanne <gugulethu2013sonni@gmail.com>
2023-08-13 12:41:50 -0700

    Printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-five
+#include <stdio.h>
+/**
+ * main - A program that print a line using the printf function
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper gramar, but the outcome is a piece of art,\n")
+		return (0);
+}
