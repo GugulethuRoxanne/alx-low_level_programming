49dca7f068431b11c058ff6e0fac68cc6369d1ac
GugulethuRoxanne <gugulethu2013sonni@gmail.com>
2023-09-06 17:01:26 -0700

    1-strdup.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,10 +8,8 @@ char **strtow(char *str);
 /**
  * word_len - locates the index marking the end of the first word contained within a string.
  * @str: the string to be searched
- *
  * Return: the index marking the end of the initial word pointed to by str
  */
-
 int word_len(char *str)
 {
 	int index = 0, len = 0;
@@ -21,17 +19,14 @@ int word_len(char *str)
 		len++;
 		index++;
 	}
-
 	return (len);
 }
 
 /**
  * count_words - counts the number of words contained within a string
  * @str: the string to be searched
- *
  * Return: the number of words contained within str
  */
-
 int count_words(char *str)
 {
 	int index = 0, words = 0, len = 0;
@@ -47,18 +42,15 @@ int count_words(char *str)
 			index += word_len(str + index);
 		}
 	}
-
 	return (words);
 }
 
 /**
  * strtow - splits a string into words
  * @str: the string to be split
- *
  * Return: if str = NULL,str = "", or the function fails - NULL
  * Otherwise - a pointer to an array of strings (words).
  */
-
 char **strtow(char *str)
 {
 	char **strings;
@@ -81,24 +73,19 @@ char **strtow(char *str)
 			index++;
 
 		letters = word_len(str + index);
-
 		strings[w] = malloc(sizeof(char) * (letters + 1));
 
 		if (strings[w] == NULL)
 		{
 			for (; w >= 0; w--)
 				free(strings[w]);
-
 			free(strings);
 			return (NULL);
 		}
-
 		for (l = 0; l < letters; l++)
 			strings[w][l] = str[index++];
-
 		strings[w][l] = '\0';
 	}
 	strings[w] = NULL;
-
 	return (strings);
 }
